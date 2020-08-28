1ed1e9e799c50982ef720dca54e7cf56462d9de3
kichankim <kstory@nexon.co.kr>
2020-08-28 10:16:37 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!!");
+        //return은 화면 이름 실제 ex: localhost:8080/hello
+        return "hello";
+    }
+}
